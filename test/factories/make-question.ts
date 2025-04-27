50adf90d32d666ade6a41bc51ed35319ce6f4398
venturions <alex.eduardo10@hotmail.com>
2025-04-27 20:55:59 -0300

    Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,24 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from 'core/entities/unique-entity-id'
+
 import {
   Question,
   QuestionProps,
 } from 'domain/forum/enterprise/entities/question'
-import { Slug } from 'domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
