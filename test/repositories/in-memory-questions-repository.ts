bf1eaab7f4709c1a874844c8bc00daaea1f93491
venturions <alex.eduardo10@hotmail.com>
2025-04-28 09:21:30 -0300

    Caso de uso: Deletar pergunta

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -8,12 +8,27 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     this.items.push(question)
   }
 
-  async findBySlug(slug: string): Promise<Question | null> {
+  async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
     if (!question) {
       return null
     }
     return question
+  }
+
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
