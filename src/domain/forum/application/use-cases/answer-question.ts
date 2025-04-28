20f49e2c5d5fe4e034e2ad44a511f3bb5e7c2564
venturions <alex.eduardo10@hotmail.com>
2025-04-28 15:06:16 -0300

    Refatorando casos de uso

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from 'core/entities/unique-entity-id'
 import { Answer } from 'domain/forum/enterprise/entities/answer'
 import { AnswerRepository } from '../repositories/answers-repository'
+import { Either, right } from 'core/either'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -8,10 +9,12 @@ interface AnswerQuestionUseCaseRequest {
   content: string
 }
 
-interface AnswerQuestionUseCaseResponse {
-  answer: Answer
-}
-
+type AnswerQuestionUseCaseResponse = Either<
+  null,
+  {
+    answer: Answer
+  }
+>
 export class AnswerQuestionUseCase {
   constructor(private answerRepository: AnswerRepository) {}
 
@@ -28,6 +31,8 @@ export class AnswerQuestionUseCase {
 
     await this.answerRepository.create(answer)
 
-    return { answer }
+    return right({
+      answer,
+    })
   }
 }
--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,14 +1,18 @@
 import { AnswerComment } from 'domain/forum/enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '../repositories/answer-comment-repository'
+import { Either, right } from 'core/either'
 
 interface FetchAnswerCommentsUseCaseRequest {
   answerId: string
   page: number
 }
 
-interface FetchAnswerCommentsUseCaseResponse {
-  answerComments: AnswerComment[]
-}
+type FetchAnswerCommentsUseCaseResponse = Either<
+  null,
+  {
+    answerComments: AnswerComment[]
+  }
+>
 
 export class FetchAnswerCommentsUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
@@ -22,8 +26,8 @@ export class FetchAnswerCommentsUseCase {
         page,
       })
 
-    return {
+    return right({
       answerComments,
-    }
+    })
   }
 }
