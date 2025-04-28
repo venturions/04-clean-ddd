7e613fb06a71607125741fac3dd3b12682630fe3
venturions <alex.eduardo10@hotmail.com>
2025-04-28 12:12:41 -0300

    Caso de uso: Deletar comentário da resposta

--- src/domain/forum/application/repositories/answer-comment-repository.ts
+++ src/domain/forum/application/repositories/answer-comment-repository.ts
@@ -2,4 +2,6 @@ import { AnswerComment } from 'domain/forum/enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
   create(answerComment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
+  delete(answerComment: AnswerComment): Promise<void>
 }
