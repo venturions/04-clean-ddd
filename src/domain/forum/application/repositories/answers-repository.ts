a11b32cb2fa2556bf6dd2d85de4b9cb657b238a9
venturions <alex.eduardo10@hotmail.com>
2025-04-28 11:31:34 -0300

    Caso de uso: Listar respostas da pergunta

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,8 +1,13 @@
+import { PaginationParams } from 'core/repositories/pagination-params'
 import { Answer } from 'domain/forum/enterprise/entities/answer'
 
 export interface AnswersRepository {
-  findById(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  findById(id: string): Promise<Answer | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<Answer[]>
   save(answer: Answer): Promise<void>
   delete(answer: Answer): Promise<void>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,8 +1,13 @@
+import { PaginationParams } from 'core/repositories/pagination-params'
 import { AnswersRepository } from 'domain/forum/application/repositories/answers-repository'
 import { Answer } from 'domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
+  async create(answer: Answer) {
+    this.items.push(answer)
+  }
 
   async findById(id: string) {
     const answer = this.items.find((item) => item.id.toString() === id)
@@ -14,8 +19,12 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer
   }
 
-  async create(answer: Answer) {
-    this.items.push(answer)
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async save(answer: Answer) {
