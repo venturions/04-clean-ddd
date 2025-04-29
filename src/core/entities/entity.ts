2a1796e2ccfcef1c42394736c308479c69e3261c
venturions <alex.eduardo10@hotmail.com>
2025-04-29 20:00:23 -0300

    Estrutura de eventos de domínio

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,17 @@ export abstract class Entity<Props> {
     this.props = props
     this._id = id ?? new UniqueEntityID()
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public equals(entity: Entity<any>) {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
+  }
 }
